92b0d07e47799cf2dfd6646a240f1bbe974d68be
mazeensaeed52 <123559692+mazeensaeed52@users.noreply.github.com>
2026-01-18 17:41:56 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,6 +3,6 @@ module.exports = (req, res) => {
 
   // Redirect back to our own server
   res.statusCode = 302;
-  res.setHeader('Location', 'https://vercel-api-nzkk.vercel.app/second');
+  res.setHeader('Location', 'https://vercel-api-nzkk.vercel.app/api/second');
   res.end();
 };
